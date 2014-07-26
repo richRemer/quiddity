292219fec450e3799df8804362ef62c35ed730ed
Rich Remer <rich.remer@gmail.com>
2014-07-25 19:12:41 -0700

    quiddity create test

--- test/test-quiddity.js
+++ test/test-quiddity.js
@@ -8,4 +8,12 @@ describe("quiddity", function() {
         expect(obj.create).to.be.a("function");
         expect(obj.init).to.be.a("function");
     });
+
+    describe("create", function() {
+        it("should derive from the current prototype", function() {
+            var obj = quiddity({}),
+                derivation = obj.create();
+            expect(obj.isPrototypeOf(derivation));
+        });
+    });
 });
